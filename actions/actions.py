e8ec1c386a703104c3325ab07c48a359f3507297
AndyNoir998 <amorenoh1@ucentral.edu.co>
2023-10-19 20:04:49 -0500

    Primer commit

--- actions/actions.py
+++ actions/actions.py
@@ -3,6 +3,7 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from typing import Any, Text, Dict, List
 import re
+import random
 
 class ActionCapturePlaca(Action):
     def name(self) -> Text:
@@ -39,7 +40,12 @@ class ActionConsultarMultas(Action):
         placa = tracker.get_slot("placa")
 
         if placa:
-            mensaje = f"Multas para la placa {placa}:\n{', '.join(multas)}"
+            # Simulamos mostrar una o dos multas de manera aleatoria
+            if random.random() < 0.5:
+                # Mostrar una multa
+                mensaje = f"Multas para la placa {placa}:\n{random.choice(multas)}"
+            else:
+                mensaje = f"No se encontraron multas para la placa {placa}."
         else:
             mensaje = "No se proporcionó una placa válida para la consulta de multas."
 
@@ -47,3 +53,30 @@ class ActionConsultarMultas(Action):
 
         return []
 
+class ActionRespuestaPreguntaFrecuente(Action):
+    def name(self) -> Text:
+        return "action_respuesta_pregunta_frecuente"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: List[Text]) -> List[Dict[Text, Any]]:
+        pregunta = tracker.latest_message.get("text")
+        
+        # Aquí puedes implementar la lógica para seleccionar la respuesta adecuada según la pregunta detectada.
+        if "requisitos para renovar la licencia de conducir" in pregunta:
+            dispatcher.utter_message("Los requisitos para renovar la licencia de conducir incluyen https://www.runt.gov.co. Puedes obtener más detalles en [sitio web].")
+        elif "plazos para registrar un vehículo" in pregunta:
+            dispatcher.utter_message("El plazo para registrar un vehículo es 60 dias apartir de la adquisición del mismo. Asegúrate de hacerlo a tiempo para evitar sanciones.")
+        # Agrega más condiciones para otras preguntas frecuentes
+        
+        return []
+
+class ActionSolicitarCita(Action):
+    def name(self) -> Text:
+        return "action_solicitar_cita"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Puedes proporcionar aquí detalles sobre cómo los usuarios pueden solicitar una cita, por ejemplo, un enlace al sitio web de citas del RUNT.
+        mensaje = "Puedes solicitar una cita para visitar el RUNT a través de nuestro sitio web https://www.runt.gov.co. Simplemente selecciona la opción de programar cita e ingresa tus datos."
+        dispatcher.utter_message(text=mensaje)
+
+        return []
+    
